3f4172073ce28b0b99004a239fea77b9762e42a6
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-09-22 10:53:08 +0530

    setup nextconfig to build package
    Signed-off-by: Sudhanshu Dasgupta <dasguptashivam23@gmail.com>

--- contact-ui/next.config.js
+++ contact-ui/next.config.js
@@ -1,20 +1,6 @@
-const compose = require("next-compose");
-const webpack = require("webpack");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
 
-module.exports = compose([
-  {
-    experimental: { esmExternals: true },
-  },
-  {
-    webpack: config => {
-      const buildEnvironment = process.env.NODE_ENV;
-      const env = new webpack.EnvironmentPlugin({
-        "process.env.NODE_ENV": process.env.NODE_ENV || "production",
-        "process.env.BUILD_ENV": buildEnvironment,
-        
-      });
-      config.plugins = [...config.plugins, env];
-      return config;
-    }
-  }
-]);
+module.exports = nextConfig;
